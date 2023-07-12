610f8181dcbba10fdf0e3168f44799cd316b23c5
Davis Bryan <138815469+DarckWar@users.noreply.github.com>
2023-07-12 18:05:02 -0300

    Update Rascunhoclasseview.py
    add Map 
    add RadButtom map padão e map satelite

--- Rascunhoclasseview.py
+++ Rascunhoclasseview.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
+from tkintermapview import TkinterMapView
 
 class app_view:
     def __init__(self, root):
@@ -13,76 +13,145 @@ class app_view:
         self.label_nome_app.pack()
         self.frame_superior.grid(row=0, column=0)
 
-        # Frame esquerdo com fundo verde
-        self.frame_esquerdo = tk.Frame(self.root,bg="green")
-        self.frame_esquerdo.grid(row=1, column=0,columnspan=2, sticky="nsew")
+        # Frame esquerdo com mapa
+        self.frame_esquerdo = tk.Frame(self.root)
+        self.mapview = TkinterMapView(self.frame_esquerdo, width=800, height=600, corner_radius=0)
+        self.mapview.set_position(-5.7936, -35.1989)
+        self.mapview.set_zoom(10)
+        self.mapview.pack(fill=tk.BOTH, expand=True)
+        self.frame_esquerdo.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
-        # Frame direito com comboboxes, radiobutton e botão
-        self.frame_direito = tk.Frame(self.root,bg="light grey")
+        # Frame direito com comboboxes, radiobuttons e botão
+        self.frame_direito = tk.Frame(self.root, bg="light grey")
         self.comboboxes_frame = tk.Frame(self.frame_direito)
         self.combobox_label_produtos = tk.Label(self.comboboxes_frame, text="Produtos")
         self.combobox_label_produtos.grid(row=0, column=0, sticky=tk.W)
-        self.combobox_produtos = ttk.Combobox(self.comboboxes_frame, values=["Selecione um produto", "item 2", "item 3"])
+        self.combobox_produtos = ttk.Combobox(
+            self.comboboxes_frame, values=["Selecione um produto", "item 2", "item 3"]
+        )
         self.combobox_produtos.current(0)
         self.combobox_produtos.grid(row=0, column=1, sticky=tk.W)
 
         self.combobox_label_mercados = tk.Label(self.comboboxes_frame, text="Mercados")
         self.combobox_label_mercados.grid(row=1, column=0, sticky=tk.W)
-        self.combobox_mercados = ttk.Combobox(self.comboboxes_frame, values=["Selecione uma loja", "item 2", "item 3"])
+        self.combobox_mercados = ttk.Combobox(
+            self.comboboxes_frame, values=["Selecione uma loja", "item 2", "item 3"]
+        )
         self.combobox_mercados.current(0)
         self.combobox_mercados.grid(row=1, column=1, sticky=tk.W)
-
-        self.combobox_label_marcas = tk.Label(self.comboboxes_frame, text="Marcas")
-        self.combobox_label_marcas.grid(row=2, column=0, sticky=tk.W)
-        self.combobox_marcas = ttk.Combobox(self.comboboxes_frame, values=["Selecione uma marca", "item 2", "item 3"])
-        self.combobox_marcas.current(0)
-        self.combobox_marcas.grid(row=2, column=1, sticky=tk.W)
 
         self.comboboxes_frame.grid(row=0, column=0, sticky="ew")
         self.radio_var = tk.StringVar()
         self.radio_var.set("Menor preço")
-        self.radio_frame = tk.Frame(self.frame_direito,bg="light grey")
-        self.radio_label = tk.Label(self.radio_frame,bg="light grey", text="Ordenar por:")
+        self.radio_frame = tk.Frame(self.frame_direito, bg="light grey")
+        self.radio_label = tk.Label(
+            self.radio_frame, bg="light grey", text="Ordenar por:"
+        )
         self.radio_label.grid(row=0, column=0, sticky=tk.W)
-        self.radio_button1 = tk.Radiobutton(self.radio_frame,bg="light grey", text="Menor preço", variable=self.radio_var,
-                                            value="Menor preço")
+        self.radio_button1 = tk.Radiobutton(
+            self.radio_frame,
+            bg="light grey",
+            text="Menor preço",
+            variable=self.radio_var,
+            value="Menor preço",
+        )
         self.radio_button1.grid(row=0, column=1, sticky=tk.W)
-        self.radio_button2 = tk.Radiobutton(self.radio_frame,bg="light grey", text="Maior preço", variable=self.radio_var,
-                                            value="Maior preço")
+        self.radio_button2 = tk.Radiobutton(
+            self.radio_frame,
+            bg="light grey",
+            text="Maior preço",
+            variable=self.radio_var,
+            value="Maior preço",
+        )
         self.radio_button2.grid(row=1, column=1, sticky=tk.W)
-        self.radio_button3 = tk.Radiobutton(self.radio_frame,bg="light grey", text="Menor peso", variable=self.radio_var,
-                                            value="Menor peso")
+        self.radio_button3 = tk.Radiobutton(
+            self.radio_frame,
+            bg="light grey",
+            text="Menor peso",
+            variable=self.radio_var,
+            value="Menor peso",
+        )
         self.radio_button3.grid(row=2, column=1, sticky=tk.W)
-        self.radio_button4 = tk.Radiobutton(self.radio_frame,bg="light grey", text="Maior peso", variable=self.radio_var,
-                                            value="Maior peso")
+        self.radio_button4 = tk.Radiobutton(
+            self.radio_frame,
+            bg="light grey",
+            text="Maior peso",
+            variable=self.radio_var,
+            value="Maior peso",
+        )
         self.radio_button4.grid(row=3, column=1, sticky=tk.W)
-        self.radio_button5 = tk.Radiobutton(self.radio_frame,bg="light grey", text="Custo benefício", variable=self.radio_var,
-                                            value="Custo benefício")
+        self.radio_button5 = tk.Radiobutton(
+            self.radio_frame,
+            bg="light grey",
+            text="Custo benefício",
+            variable=self.radio_var,
+            value="Custo benefício",
+        )
         self.radio_button5.grid(row=4, column=1, sticky=tk.W)
         self.radio_frame.grid(row=1, column=0, sticky="w")
 
-        self.filtrar_button = tk.Button(self.frame_direito,bg="light grey", text="Filtrar", command=self.filtrar_items)
-        self.filtrar_button.grid(row=2, column=0)
+        self.map_type_var = tk.StringVar()
+        self.map_type_var.set("Padrão")
+        self.map_type_frame = tk.Frame(self.frame_direito, bg="light grey")
+        self.map_type_label = tk.Label(
+            self.map_type_frame, bg="light grey", text="Tipo de Mapa:"
+        )
+        self.map_type_label.grid(row=0, column=0, sticky=tk.W)
+        self.map_type_button1 = tk.Radiobutton(
+            self.map_type_frame,
+            bg="light grey",
+            text="Padrão",
+            variable=self.map_type_var,
+            value="Padrão",
+            command=self.mudar_tipo_mapa,
+        )
+        self.map_type_button1.grid(row=0, column=1, sticky=tk.W)
+        self.map_type_button2 = tk.Radiobutton(
+            self.map_type_frame,
+            bg="light grey",
+            text="Satélite",
+            variable=self.map_type_var,
+            value="Satélite",
+            command=self.mudar_tipo_mapa,
+        )
+        self.map_type_button2.grid(row=1, column=1, sticky=tk.W)
+        self.map_type_frame.grid(row=2, column=0, sticky="w")
+
+        self.filtrar_button = tk.Button(
+            self.frame_direito,
+            bg="light grey",
+            text="Filtrar",
+            command=self.filtrar_items,
+        )
+        self.filtrar_button.grid(row=3, column=0)
 
         self.frame_direito.grid(row=1, column=1, sticky="e")
 
         # Frame inferior com Treeview
-        self.frame_inferior = Tabela(self.root, ["Produto", "Marca", "Mercado", "Preço", "Peso"])
+        self.frame_inferior = Tabela(
+            self.root, ["Produto", "Marca", "Mercado", "Preço", "Peso"]
+        )
         self.frame_inferior.grid(row=2, column=0, columnspan=2, sticky="nsew")
 
         self.root.grid_rowconfigure(1, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
 
+    def mudar_tipo_mapa(self):
+        tipo_mapa = self.map_type_var.get()
+
+        if tipo_mapa == "Satélite":
+            self.mapview.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+        elif tipo_mapa == "Padrão":
+            self.mapview.set_tile_server("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png")
+
     def filtrar_items(self):
         item_produtos = self.combobox_produtos.get()
         item_mercados = self.combobox_mercados.get()
-        item_marcas = self.combobox_marcas.get()
         ordenacao = self.radio_var.get()
 
         print("Item selecionado no combobox de Produtos:", item_produtos)
         print("Item selecionado no combobox de Mercados:", item_mercados)
-        print("Item selecionado no combobox de Marcas:", item_marcas)
         print("Opção selecionada no radiobutton:", ordenacao)
 
 
@@ -93,32 +162,40 @@ class Tabela(tk.Frame):
         self._inicializa_gui(pai)
 
     def _inicializa_gui(self, pai):
-        self._tv = ttk.Treeview(self, columns=self._nomes_cols, show='headings')
-        self._sb_y = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self._tv.yview)
+        self._tv = ttk.Treeview(
+            self, columns=self._nomes_cols, show="headings"
+        )
+        self._sb_y = ttk.Scrollbar(
+            self, orient=tk.VERTICAL, command=self._tv.yview
+        )
         self._tv.configure(yscroll=self._sb_y.set)
-        self._sb_x = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=self._tv.xview)
+        self._sb_x = ttk.Scrollbar(
+            self, orient=tk.HORIZONTAL, command=self._tv.xview
+        )
         self._tv.configure(xscroll=self._sb_x.set)
 
         for tit in self._nomes_cols:
             self._tv.heading(tit, text=tit)
             self._tv.column(tit, width=90, minwidth=100)
 
-        self._tv.grid(row=0, column=0, sticky='nsew')
-        self._sb_y.grid(row=0, column=1, sticky='ns')
-        self._sb_x.grid(row=1, column=0, columnspan=1, sticky='we')
+        self._tv.grid(row=0, column=0, sticky="nsew")
+        self._sb_y.grid(row=0, column=1, sticky="ns")
+        self._sb_x.grid(row=1, column=0, columnspan=1, sticky="we")
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
     def adiciona_dado(self, strings_cols):
         if len(strings_cols) != len(self._nomes_cols):
-            raise Exception(f'Lista deve conter {len(self._nomes_cols)} strings (lista passada contém {len(strings_cols)})')
+            raise Exception(
+                f'Lista deve conter {len(self._nomes_cols)} strings (lista passada contém {len(strings_cols)})'
+            )
 
-        self._tv.insert('', tk.END, values=strings_cols)
+        self._tv.insert("", tk.END, values=strings_cols)
 
 
 root = tk.Tk()
-root.title('View em App')
-root.geometry('600x480+100+100')
+root.title("View em App")
+root.geometry("600x480+100+100")
 app = app_view(root)
 root.mainloop()
