ab013e6cb5c1416a36ad53d023f7c9049170fe46
Luis Martins <chateadorn@gmail.com>
2023-07-17 20:37:59 -0300

    identacao

--- controle_v2.py
+++ controle_v2.py
@@ -5,19 +5,19 @@ from Testeview_v2 import app_view, Produtos_dados, Mercado_local
 class Controle:
     def __init__(self):
         self.model = Model()
-        root = tk.Tk()
-        root.title("View em App")
-        root.geometry("600x480+100+100")
+        self.root = tk.Tk()
+        self.root.title("View em App")
+        self.root.geometry("600x480+100+100")
         self.lista_lojas = self.pegar_lista_lojas()
         self.lista_produtos = self.pegar_lista_produtos()
         self.lista_dados = self.pegar_todos_produtos_dados()
         self.lista_localizacoes = self.pegar_lista_lojas_coord()
-        self.view = app_view(root, self.lista_lojas, self.lista_produtos, self.lista_dados, self.lista_localizacoes)
+        self.view = app_view(self.root, self.lista_lojas, self.lista_produtos, self.lista_dados, self.lista_localizacoes)
 
     def inicializa_app(self):
-    	self.view.inicializar()
-    	self.botao_filtro()
-    	self.root.mainloop()
+        self.view.inicializar()
+        self.botao_filtro()
+        self.root.mainloop()
 
     def pegar_lista_lojas(self):
         lista_lojas = self.model.lista_mercados()
@@ -49,22 +49,27 @@ class Controle:
         mercado_encontrado = self.model.encontra_mercado(mercado)
         coordenadas = mercado_encontrado.localizacao()
         return coordenadas
-
+    
     def pegar_lista_lojas_coord(self):
-    	lista_lojas = self.pegar_lista_lojas()
-    	lista_coord = []
-    	for mercado in lista_lojas:
-    		mercado_encontrado = Mercado_local(mercado,self.encontrar_mercado_e_localizacao(mercado))
-    		lista_coord.append(mercado_encontrado)
-    	return lista_coord
+        lista_lojas = self.pegar_lista_lojas()
 
+        lista_coord = []
+
+        for mercado in lista_lojas:  
+            mercado_encontrado = Mercado_local(mercado, self.encontrar_mercado_e_localizacao(mercado))
+            lista_coord.append(mercado_encontrado)
+              
+        return lista_coord
+         
     def botao_filtro(self):
     	if self.view.filtrar_items() != "normal":
-    		nova_list_produtos = []
-    		dados = self.encontrar_produtos_iguais(self.view.filtrar_items())
-    		for produto_dados in dados:
+            nova_list_produtos = []
+            dados = self.encontrar_produtos_iguais(self.view.filtrar_items())
+            
+            for produto_dados in dados:
                 produto_new_lis = Produtos_dados(produto_dados.descricao,produto_dados.mercado,produto_dados.unit_value)
                 nova_list_produtos.append(produto_new_lis)
+            
             self.view.atualiza_tabela(nova_list_produtos)
 def main():
 	testcontrole = Controle()
