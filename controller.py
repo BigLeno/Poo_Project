0e2bc5debeb8dabfc13d0d9eb20aa951b71aaa57
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2023-07-18 01:14:35 -0300

    atualizando o controller

--- controller.py
+++ controller.py
@@ -1,41 +1,84 @@
-from model_v2 import Model
+import tkinter as tk
+from model import Model
+from view import View
 
-class Controle:
+class Controller(Model, View):
+    def __init__(self) -> None:
+        """Construtor da classe Controller"""
+        Model.__init__(self)
+        View.__init__(self)
+        print("Iniciando o módulo Controller")
+        self.nome_lojas = []
+        self.razao_produtos = []
+        self.carrega_bd()
+        print("\nAplicativo iniciado com sucesso!")
 
-    def __init__(self):
-        self.model = Model()
+    def carrega_bd(self) -> None:
+        """Método que carrega os itens de acordo com o pedido"""
+        self.lista_geral_mercados = self.lista_mercados()
+        self.lista_geral_produtos = self.lista_produtos()
 
-    def lista_lojas(self, lista_lojas):
-        lojas = self.model.lista_mercados()
-        lista_lojas.extend(lojas)
+    def acessa_dados_mercados(self) -> None:
+        """Método que separa o nome da localização do mercado"""
+        for mercado in self.lista_geral_mercados:
+            self.nome_lojas.append(mercado.nome)
 
-    def lista_produtos(self, lista_produtos):
-        produtos = self.model.lista_produtos()
-        lista_produtos.extend(produtos)
+    def acessa_dados_produtos(self) -> None:
+        """Método que separa o nome do produto"""
+        for produto in self.lista_geral_produtos:
+            self.razao_produtos.append((produto.descricao))
 
-    def filtro_produtos(self, produto, lista_dados):
+    def inicializa_app(self) -> None:
+        """Método que inicializa o App"""
+        self.carrega_bd()
+        self.acessa_dados_mercados()
+        self.acessa_dados_produtos()
+        self.inicializar(self.nome_lojas, self.razao_produtos, self.lista_geral_mercados)
+        self.inicializa_tabela()
+        self.root.mainloop()
+    
+    def inicializa_tabela(self) -> None:
+        """Inicia o widget tabela"""
+        for item in self.lista_geral_produtos:
+            listTest = [item.descricao, item.mercado, item.unit_value]
+            self.frame_inferior.adiciona_dado(listTest)
 
-        produtos_encontrados = self.model.encontra_produto(produto)
+    def filtro_marcadores(self, item_produtos) -> None:
+        """Método que filtra os marcadores"""
+        for produto in self.lista_geral_produtos:
+            if produto.descricao == item_produtos:
+                for mercado in self.lista_geral_mercados:
+                    if produto.mercado == mercado.nome:
+                        self.criar_marcador(mercado.localizacao(), mercado.nome)  
+
+    def filtrar_items(self) -> None:
+        """Método que gere as ações do filtro"""
+        item_produtos = self.combobox_produtos.get()
+        item_lojas = self.combobox_mercados.get()
+
+        if item_produtos != "Selecione um produto":
+            self.inicializa_tabela()
+            self.frame_inferior.remover_linhas(item_produtos, "Produto")
+            if item_lojas == "Selecione uma loja":
+                self.mapview.delete_all_marker()
+                self.filtro_marcadores(item_produtos)
+                self.filtro_combobox(item_produtos) 
+
+        elif item_lojas != "Selecione uma loja":
+            self.mapview.delete_all_marker()
+            for mercado in self.lista_geral_mercados:
+                if mercado.nome == item_lojas:
+                    self.criar_marcador(mercado.localizacao(),mercado.nome)
+            self.frame_inferior.remover_linhas(item_lojas, "Mercado")
         
-        for produto in produtos_encontrados:
-            lista_dados.append([produto.descricao, produto.mercado, produto.unit_value])
-
-    def filtro_lojas(self, mercado):
-        mercado_encontrado = self.model.encontra_mercado(mercado)
-        coordenadas = mercado_encontrado.localizacao()
-        self.view.criar_marcador(coordenadas, mercado)
-
-    def consultar_localizacao(self, mercado):
-        mercado_achei = self.model.encontra_mercado(mercado)
-        coordenadas = mercado_achei.localizacao()
-        x, y = coordenadas
-        lista_coordenadas = [x, y]
-        return lista_coordenadas
+        else:
+            self.inicializa_marcadores()
+            self.inicializa_tabela()
 
 
-class dados:
-    def __init__(self, arg):
-        super(dados, self).__init__()
+def main():
+	Control = Controller()
+	Control.inicializa_app()
 
-        self.arg = arg
-        
+if __name__ == '__main__':
+	main()
